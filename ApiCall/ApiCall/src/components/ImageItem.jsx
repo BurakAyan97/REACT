77471b120a5fb146f5dff31e77437d161064e81d
BurakAyan97 <burakayan97@outlook.com>
2023-06-20 22:08:04 +0300

    updated

--- ApiCall/ApiCall/src/components/ImageItem.jsx
+++ ApiCall/ApiCall/src/components/ImageItem.jsx
@@ -1,5 +1,13 @@
 function ImageItem({ image }) {
-  return <div>console.log({image});</div>;
+  return (
+    <div>
+      <img
+        className="imageListImg"
+        src={image.urls.small}
+        alt={image.alt_description}
+      />
+    </div>
+  );
 }
 
 export default ImageItem;
--- ApiCall/ApiCall/src/components/ImageList.jsx
+++ ApiCall/ApiCall/src/components/ImageList.jsx
@@ -2,7 +2,7 @@ import ImageItem from "./ImageItem";
 
 function ImageList({ imagesPlaceHolder }) {
   return (
-    <div>
+    <div className="imageList">
       {imagesPlaceHolder.map((image, index) => {
         return <ImageItem key={index} image={image} />; //key vermek zorunlu yoksa hata veriyor.API den gelen resim listesini ImageItem componentine props olarak verdik. Her biri bir component mantığı ile çalışsın diye.
       })}
