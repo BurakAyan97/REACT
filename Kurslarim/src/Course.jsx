f6662fdd93c649f47c8f60377d8e8cff424a7426
BurakAyan97 <burakayan97@outlook.com>
2023-06-22 13:17:02 +0300

    updated

---
+++ Kurslarim/src/Course.jsx
@@ -0,0 +1,20 @@
+function Course({ id, content, title, price, removeOneCourse }) {
+  //   console.log(id);
+  //   console.log(content);
+  //   console.log(title);
+  //   console.log(price);
+  return (
+    <div className="card">
+      <div className="cardTitlePrice">
+        <h2 className="cardTitle">{title}</h2>
+        <h4 className="cardPrice">{price}₺</h4>
+      </div>
+      <p>{content}</p>
+      <button className="cardDeleteBtn" onClick={() => removeOneCourse(id)}>
+        Sil
+      </button>
+    </div>
+  );
+}
+
+export default Course;
---
+++ Kurslarim/src/Loading.jsx
@@ -0,0 +1,5 @@
+function Loading() {
+  return <div>Loading...</div>; //Dosyalar gelene kadar loading ekranı gözüksün diye uğraşıyıroz.
+}
+
+export default Loading;
